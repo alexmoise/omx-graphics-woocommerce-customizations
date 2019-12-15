f3ecdad47d11d50c7ba820e5287d13381c632b32
Alex Moise <alex@moise.pro>
2019-12-15 09:13:48 +0200

    Create omx-graphics-woocommerce-customizations.php

---
+++ omx-graphics-woocommerce-customizations.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: OMX Graphics Woocommerce customizations
+
+ * Plugin URI: https://github.com/alexmoise/omx-graphics-woocommerce-customizations
+ * GitHub Plugin URI: https://github.com/alexmoise/omx-graphics-woocommerce-customizations
+ * Description: A custom plugin to add required ACF components to OMX Graphics Woocommerce shop and to style the front end as required. Works based on ACF & ACF Pro & ACF Extended plugins and requires Woocommerce and Astra theme. For details/troubleshooting please contact me at <a href="https://moise.pro/contact/">https://moise.pro/contact/</a>
+ * Version: 0.1
+ * Author: Alex Moise
+ * Author URI: https://moise.pro
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {	exit(0);}
+
+
+?>
