ebaef320479e2ef6b9c9132b0d0bb51e9b0d95b1
Alex <alex@moise.pro>
2019-12-19 18:27:31 +0200

    Adding CSS file and various JS interactions
    ... also leave the breadcrumbs alone because the requested design makes a good use of the original ones.

--- omx-graphics-woocommerce-customizations.php
+++ omx-graphics-woocommerce-customizations.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://github.com/alexmoise/omx-graphics-woocommerce-customizations
  * GitHub Plugin URI: https://github.com/alexmoise/omx-graphics-woocommerce-customizations
  * Description: A custom plugin to add required ACF components to OMX Graphics Woocommerce shop and to style the front end as required. Works based on ACF & ACF Pro & ACF Extended plugins and requires Woocommerce and Astra theme. For details/troubleshooting please contact me at <a href="https://moise.pro/contact/">https://moise.pro/contact/</a>
- * Version: 0.2
+ * Version: 0.3
  * Author: Alex Moise
  * Author URI: https://moise.pro
  */
@@ -16,16 +16,21 @@ if ( ! defined( 'ABSPATH' ) ) {	exit(0);}
 // Display a debug text, for control
 add_action( 'woocommerce_product_meta_end', 'moomx_display_dbg_for_products', 90 );
 function moomx_display_dbg_for_products() { echo 'DBG 15'; }
-
 // Load our own JS
 add_action( 'wp_enqueue_scripts', 'moomx_adding_scripts', 9999999 );
 function moomx_adding_scripts() {
 	wp_register_script('omxgwc-script', plugins_url('omxgwc.js', __FILE__), array('jquery'), '', true);
 	wp_enqueue_script('omxgwc-script');
 }
+// Load our own CSS
+add_action( 'wp_enqueue_scripts', 'moomx_adding_styles', 9999999 );
+function moomx_adding_styles() {
+	wp_register_style('omxgwc-styles', plugins_url('omxgwc.css', __FILE__));
+	wp_enqueue_style('omxgwc-styles');
+}
 // Add the breadcrumbs in the right place (by default displayed in Product Summary, so removed in Customizer in the first place)
-add_action('template_redirect', 'moomx_prod_breadcrumbs', 10 );
-function moomx_prod_breadcrumbs(){ add_action( 'woocommerce_before_single_product', 'woocommerce_breadcrumb', 1 ); }
+// add_action('template_redirect', 'moomx_prod_breadcrumbs', 10 );
+// function moomx_prod_breadcrumbs(){ add_action( 'woocommerce_before_single_product', 'woocommerce_breadcrumb', 1 ); }
 // Remove the product price
 add_filter( 'woocommerce_get_price_html', function ($price) { return ''; } );
 // Remove hover zoom
--- omxgwc.js
+++ omxgwc.js
@@ -1,13 +1,56 @@
 /** 
  * JS functions for OMX Graphics Woocommerce customizations plugin
- * Version 0.2
+ * Version 0.3
  * (version above is equal with main plugin file version when this file was updated)
  */
 
 // Let's have it tested first (will remove this after a while)
-jQuery(document).ready(function() { console.log('JS Loaded - 02'); });
+jQuery(document).ready(function() { console.log('JS Loaded - 15'); });
 
-// Dynamically change the font of Rider Name in its input field
-jQuery('#wccf_product_field_name_style').change(function(e) {
-	jQuery('#wccf_product_field_rider_name').css('font-family', (this.value) );
+
+// Some stuff to do when document.ready:
+jQuery(document).ready(function() {
+	// Add the span elements needed to add the styled name and number
+	jQuery('#wccf_product_field_name_style_container li > label').after('<span class="styled ridernumber" style="margin-left:10px"></span>').after('<span class="styled ridername" style="margin-left:10px"></span>');
+	// Insert the initial colors in color pickers
+	var iniColor = '#3a3a3a'; jQuery('#wccf_product_field_number_color').val(iniColor).css("border-left-color",iniColor);
+	var plateColor = '#dfdfdf'; jQuery('#wccf_product_field_number_plate_color').val(plateColor).css("border-left-color",plateColor);
+});
+// Add the font CSS style for each syled name and number
+jQuery(document).ready(function() {
+	jQuery('#wccf_product_field_name_style_container li > input').each(function() {
+		eachStyle = jQuery(this).attr('value');
+		jQuery('#wccf_product_field_name_style_container li > input[value="'+eachStyle+'"]').siblings('span.styled').css("font-family",eachStyle);
+	});
+});
+
+// Dynamically change the font of Rider Name and Number in its input field
+jQuery('#wccf_product_field_name_style_container input').change(function(e) {
+	var chosenstyle = jQuery(this).val();
+	jQuery('#wccf_product_field_rider_name').css('font-family', chosenstyle );
+	jQuery('#wccf_product_field_rider_number').css('font-family', chosenstyle );	
+});
+
+// Dynamically change the Rider Name in styles list
+jQuery("#wccf_product_field_rider_name").on('input', function(e) {
+	setTimeout(function() {
+		jQuery(".styled.ridername").empty().text( jQuery("#wccf_product_field_rider_name").val() );
+	}, 1500);
+});
+// Dynamically change the Rider Number in styles list
+jQuery("#wccf_product_field_rider_number").on('input', function(e) {
+	setTimeout(function() {
+		jQuery(".styled.ridernumber").empty().text( jQuery("#wccf_product_field_rider_number").val() );
+	}, 1500);
+});
+
+// Dynamically change the color of rider number
+jQuery('#wccf_product_field_number_color').change(function(e) {
+	var chosencolor = jQuery(this).val();
+	jQuery('#wccf_product_field_rider_number').css('color', chosencolor );	
+});
+// Dynamically change the background of number plate
+jQuery('#wccf_product_field_number_plate_color').change(function(e) {
+	var chosenbkcolor = jQuery(this).val();
+	jQuery('#wccf_product_field_rider_number').css('background-color', chosenbkcolor );	
 });
