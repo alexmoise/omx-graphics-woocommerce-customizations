df7fffca5b54c7505e00c5ae8a10020c7c58f26f
Alex <alex@moise.pro>
2019-12-19 18:31:09 +0200

    Removing the ACF reference from Readme

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # OMX Graphics customizations plugin
-A custom plugin to add required ACF components to OMX Graphics Woocommerce shop and to style the front end as required. Works based on ACF & ACF Pro & ACF Extended plugins and requires Woocommerce and Astra theme. For details/troubleshooting please contact me at <a href="https://moise.pro/contact/">https://moise.pro/contact/</a>
+A custom plugin to add required customizations to OMX Graphics Woocommerce shop and to style the front end as required. Works based on WooCommerce Custom Fields plugin by RightPress and requires Woocommerce and Astra theme. For details/troubleshooting please contact me at <a href="https://moise.pro/contact/">https://moise.pro/contact/</a>
--- omx-graphics-woocommerce-customizations.php
+++ omx-graphics-woocommerce-customizations.php
@@ -4,8 +4,8 @@
 
  * Plugin URI: https://github.com/alexmoise/omx-graphics-woocommerce-customizations
  * GitHub Plugin URI: https://github.com/alexmoise/omx-graphics-woocommerce-customizations
- * Description: A custom plugin to add required ACF components to OMX Graphics Woocommerce shop and to style the front end as required. Works based on ACF & ACF Pro & ACF Extended plugins and requires Woocommerce and Astra theme. For details/troubleshooting please contact me at <a href="https://moise.pro/contact/">https://moise.pro/contact/</a>
- * Version: 0.3
+ * Description: A custom plugin to add required customizations to OMX Graphics Woocommerce shop and to style the front end as required. Works based on WooCommerce Custom Fields plugin by RightPress and requires Woocommerce and Astra theme. For details/troubleshooting please contact me at <a href="https://moise.pro/contact/">https://moise.pro/contact/</a>
+ * Version: 0.4
  * Author: Alex Moise
  * Author URI: https://moise.pro
  */
