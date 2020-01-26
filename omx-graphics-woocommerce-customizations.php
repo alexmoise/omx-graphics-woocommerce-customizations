5ba7a2d82ecf64b33f6d5ebb464f5139829a8abe
Alex <alex@moise.pro>
2020-01-26 20:44:33 +0200

    Few final touches to page container

--- omx-graphics-woocommerce-customizations.php
+++ omx-graphics-woocommerce-customizations.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://github.com/alexmoise/omx-graphics-woocommerce-customizations
  * GitHub Plugin URI: https://github.com/alexmoise/omx-graphics-woocommerce-customizations
  * Description: A custom plugin to add required customizations to OMX Graphics Woocommerce shop and to style the front end as required. Works based on WooCommerce Custom Fields plugin by RightPress and requires Woocommerce and Astra theme. For details/troubleshooting please contact me at <a href="https://moise.pro/contact/">https://moise.pro/contact/</a>
- * Version: 0.31
+ * Version: 0.32
  * Author: Alex Moise
  * Author URI: https://moise.pro
  */
@@ -14,7 +14,7 @@ if ( ! defined( 'ABSPATH' ) ) {	exit(0);}
 
 // === Various WC Customizations below:
 // Display a debug text, for control
-add_action( 'woocommerce_product_meta_end', 'moomx_display_dbg_for_products', 90 );
+// add_action( 'woocommerce_product_meta_end', 'moomx_display_dbg_for_products', 90 );
 function moomx_display_dbg_for_products() { echo 'DBG 18'; }
 
 // === Increase image quality a bit, so all the straight lines appears smooth
