5f0a69d55f390396bea5cf9332e612e199e27e07
Alex <alex@moise.pro>
2020-03-04 14:32:09 +0200

    Improved option field targeting for dynamically added options

--- omx-graphics-woocommerce-customizations.php
+++ omx-graphics-woocommerce-customizations.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://github.com/alexmoise/omx-graphics-woocommerce-customizations
  * GitHub Plugin URI: https://github.com/alexmoise/omx-graphics-woocommerce-customizations
  * Description: A custom plugin to add required customizations to OMX Graphics Woocommerce shop and to style the front end as required. Works based on WooCommerce Custom Fields plugin by RightPress and requires Woocommerce and Astra theme. For details/troubleshooting please contact me at <a href="https://moise.pro/contact/">https://moise.pro/contact/</a>
- * Version: 0.48
+ * Version: 0.49
  * Author: Alex Moise
  * Author URI: https://moise.pro
  */
@@ -37,10 +37,10 @@ function moomx_only_dashes_in_custom_fields_options() {
 	echo '
 	<script>
 		jQuery(document).ready(function() {
-			jQuery(".wccf_post_option_wrapper").on("keyup", "input.wccf_post_config_options_key", function(event) {
+			jQuery(".wccf_post_options").on("keyup", "input.wccf_post_config_options_key", function(event) {
 				this.value = this.value.replace(/ /g, "_");
 			});
-			jQuery(".wccf_post_option_wrapper").on("keyup", "input.wccf_post_config_options_key", function(event) {
+			jQuery(".wccf_post_options").on("keyup", "input.wccf_post_config_options_key", function(event) {
 				this.value = this.value.replace(/-/g, "_");
 			});
 		});
