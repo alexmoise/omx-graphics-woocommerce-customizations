c02884264deddbda0f80c0137d3c953a92b89e7f
Alex Moise <alex@moise.pro>
2020-05-13 14:59:58 +0300

    Send failed Order email also to Customer

--- omx-graphics-woocommerce-customizations.php
+++ omx-graphics-woocommerce-customizations.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://github.com/alexmoise/omx-graphics-woocommerce-customizations
  * GitHub Plugin URI: https://github.com/alexmoise/omx-graphics-woocommerce-customizations
  * Description: A custom plugin to add required customizations to OMX Graphics Woocommerce shop and to style the front end as required. Works based on WooCommerce Custom Fields plugin by RightPress and requires Woocommerce and Astra theme. For details/troubleshooting please contact me at <a href="https://moise.pro/contact/">https://moise.pro/contact/</a>
- * Version: 1.0.3
+ * Version: 1.0.5
  * Author: Alex Moise
  * Author URI: https://moise.pro
  * WC requires at least: 3.0.0
@@ -300,5 +300,12 @@ add_filter('woocommerce_gallery_thumbnail_size', function($size) {
 		'crop' => 1,
 	);
 });
+
+// Send failed Order email also to Customer
+// add_filter( 'woocommerce_email_recipient_cancelled_order', 'moomx_cancelled_order_add_customer_email', 10, 2 );
+add_filter( 'woocommerce_email_recipient_failed_order', 'moomx_cancelled_order_add_customer_email', 10, 2 );
+function moomx_cancelled_order_add_customer_email( $recipient, $order ){
+     return $recipient . ',' . $order->billing_email;
+}
 
 ?>
