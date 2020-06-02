ce600e027ed50b09cbe9fa3639fa26985fc8f4a4
Alex Moise <alex@moise.pro>
2020-06-02 18:00:16 +0300

    Updated version of Woocommerce tested version

--- omx-graphics-woocommerce-customizations.php
+++ omx-graphics-woocommerce-customizations.php
@@ -5,11 +5,11 @@
  * Plugin URI: https://github.com/alexmoise/omx-graphics-woocommerce-customizations
  * GitHub Plugin URI: https://github.com/alexmoise/omx-graphics-woocommerce-customizations
  * Description: A custom plugin to add required customizations to OMX Graphics Woocommerce shop and to style the front end as required. Works based on WooCommerce Custom Fields plugin by RightPress and requires Woocommerce and Astra theme. For details/troubleshooting please contact me at <a href="https://moise.pro/contact/">https://moise.pro/contact/</a>
- * Version: 1.0.19
+ * Version: 1.0.20
  * Author: Alex Moise
  * Author URI: https://moise.pro
  * WC requires at least: 3.0.0
- * WC tested up to: 4.0.1
+ * WC tested up to: 4.1.1
  */
 
 if ( ! defined( 'ABSPATH' ) ) {	exit(0);}
