0672898e01caee2ac7e454308f9c29857c523ee0
Alex Moise <alex@moise.pro>
2020-07-04 17:45:03 +0300

    Removed the Checkout button in Mini Cart Widget

--- omx-graphics-woocommerce-customizations.php
+++ omx-graphics-woocommerce-customizations.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://github.com/alexmoise/omx-graphics-woocommerce-customizations
  * GitHub Plugin URI: https://github.com/alexmoise/omx-graphics-woocommerce-customizations
  * Description: A custom plugin to add required customizations to OMX Graphics Woocommerce shop and to style the front end as required. Works based on WooCommerce Custom Fields plugin by RightPress and requires Woocommerce and Astra theme. For details/troubleshooting please contact me at <a href="https://moise.pro/contact/">https://moise.pro/contact/</a>
- * Version: 1.2.6
+ * Version: 1.2.7
  * Author: Alex Moise
  * Author URI: https://moise.pro
  * WC requires at least: 3.0.0
@@ -184,6 +184,11 @@ function moomx_rearrange_woocomemrce_features() {
 	// Change position of Proceed to Checkout button (lower, so Angelleye Paypal button - on prio 22 - gets on top of it)
 	remove_action( 'woocommerce_proceed_to_checkout', 'woocommerce_button_proceed_to_checkout', 20 );
 	add_action( 'woocommerce_proceed_to_checkout', 'woocommerce_button_proceed_to_checkout', 40 );
+}
+
+// Remove Checkout button in Mini Cart Widget (by emptying its HTML) 
+if ( ! function_exists( 'woocommerce_widget_shopping_cart_proceed_to_checkout' ) ) {
+	function woocommerce_widget_shopping_cart_proceed_to_checkout() { echo ''; }
 }
 
 // Empty the Sale Flash HTML
