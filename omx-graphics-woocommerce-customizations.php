715930dcd0b56742972a33973040818975e19967
Alex Moise <alex@moise.pro>
2021-10-10 19:29:43 +0300

    Removing the bold style

--- omx-graphics-woocommerce-customizations.php
+++ omx-graphics-woocommerce-customizations.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://github.com/alexmoise/omx-graphics-woocommerce-customizations
  * GitHub Plugin URI: https://github.com/alexmoise/omx-graphics-woocommerce-customizations
  * Description: A custom plugin to add required customizations to OMX Graphics Woocommerce shop and to style the front end as required. Works based on WooCommerce Custom Fields plugin by RightPress and requires Woocommerce and Astra theme. For details/troubleshooting please contact me at <a href="https://moise.pro/contact/">https://moise.pro/contact/</a>
- * Version: 1.2.50
+ * Version: 1.2.51
  * Author: Alex Moise
  * Author URI: https://moise.pro
  * WC requires at least: 3.0.0
@@ -251,7 +251,7 @@ if ( ! function_exists( 'woocommerce_button_proceed_to_checkout' ) ) {
 // Add Agree to Terms in Cart
 add_action( 'woocommerce_proceed_to_checkout', 'moomx_cart_privacy_policy_checkbox', 1 );
 function moomx_cart_privacy_policy_checkbox() {
-	echo '<strong>By clicking payment button I agree to the <a href="/terms-conditions/" target="_blank">Terms and Conditions</a></strong>';
+	echo 'By clicking payment button I agree to the <a href="/terms-conditions/" target="_blank">Terms and Conditions</a>';
 }
 
 // Translate/change some strings as needed
