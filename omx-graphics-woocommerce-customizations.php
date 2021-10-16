44331f072cf0cb1508160a700211003e6fec662c
Alex Moise <alex@moise.pro>
2021-10-16 16:59:52 +0300

    Adding the security icons in the Cart page

--- omx-graphics-woocommerce-customizations.php
+++ omx-graphics-woocommerce-customizations.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://github.com/alexmoise/omx-graphics-woocommerce-customizations
  * GitHub Plugin URI: https://github.com/alexmoise/omx-graphics-woocommerce-customizations
  * Description: A custom plugin to add required customizations to OMX Graphics Woocommerce shop and to style the front end as required. Works based on WooCommerce Custom Fields plugin by RightPress and requires Woocommerce and Astra theme. For details/troubleshooting please contact me at <a href="https://moise.pro/contact/">https://moise.pro/contact/</a>
- * Version: 1.2.51
+ * Version: 1.2.52
  * Author: Alex Moise
  * Author URI: https://moise.pro
  * WC requires at least: 3.0.0
@@ -248,10 +248,16 @@ if ( ! function_exists( 'woocommerce_button_proceed_to_checkout' ) ) {
 	}
 }
 
+// Adding the security icons in the Cart page under the payment/checkout/save cart buttons
+add_action( 'woocommerce_proceed_to_checkout', 'moomx_security_icons', 1);
+function moomx_security_icons() {
+	echo '<div class="security-icons-container"><img class="sharp-image" src="' . plugin_dir_url( __FILE__ ).'images/SecureIconsCheckout02.png . "></div>';
+}
+
 // Add Agree to Terms in Cart
-add_action( 'woocommerce_proceed_to_checkout', 'moomx_cart_privacy_policy_checkbox', 1 );
+add_action( 'woocommerce_proceed_to_checkout', 'moomx_cart_privacy_policy_checkbox', 110 );
 function moomx_cart_privacy_policy_checkbox() {
-	echo 'By clicking payment button I agree to the <a href="/terms-conditions/" target="_blank">Terms and Conditions</a>';
+	echo '<div class="cart-terms-agreement">By clicking payment button I agree to the <a href="/terms-conditions/" target="_blank">Terms and Conditions</a></div>';
 }
 
 // Translate/change some strings as needed
