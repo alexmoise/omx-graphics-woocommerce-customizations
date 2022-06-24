561e18b472e97140871fc61a930beae532a9555f
Alex Moise <alex@moise.pro>
2022-06-24 18:55:52 +0300

    Fixing the secure payments icons image URL

--- omx-graphics-woocommerce-customizations.php
+++ omx-graphics-woocommerce-customizations.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://github.com/alexmoise/omx-graphics-woocommerce-customizations
  * GitHub Plugin URI: https://github.com/alexmoise/omx-graphics-woocommerce-customizations
  * Description: A custom plugin to add required customizations to OMX Graphics Woocommerce shop and to style the front end as required. Works based on WooCommerce Custom Fields plugin by RightPress and requires Woocommerce and Astra theme. For details/troubleshooting please contact me at <a href="https://moise.pro/contact/">https://moise.pro/contact/</a>
- * Version: 1.2.62
+ * Version: 1.2.63
  * Author: Alex Moise
  * Author URI: https://moise.pro
  * WC requires at least: 3.0.0
@@ -252,7 +252,7 @@ if ( ! function_exists( 'woocommerce_button_proceed_to_checkout' ) ) {
 add_action( 'woocommerce_proceed_to_checkout', 'moomx_security_icons', 1);
 add_action( 'woocommerce_review_order_after_submit', 'moomx_security_icons', 1);
 function moomx_security_icons() {
-	echo '<div class="security-icons-container"><img class="sharp-image" src="' . plugin_dir_url( __FILE__ ).'images/SecureIconsCheckout02.png."></div>';
+	echo '<div class="security-icons-container"><img class="sharp-image" src="' . plugin_dir_url( __FILE__ ).'images/SecureIconsCheckout02.png"></div>';
 }
 
 // Add Agree to Terms in Cart
