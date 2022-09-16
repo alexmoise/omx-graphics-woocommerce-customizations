70725998dbc41ca5f9416dfcd3f3dee807550c29
Alex Moise <alex@moise.pro>
2022-09-16 23:26:49 +0300

    Remove Save and Share cart button in Cart

--- omx-graphics-woocommerce-customizations.php
+++ omx-graphics-woocommerce-customizations.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://github.com/alexmoise/omx-graphics-woocommerce-customizations
  * GitHub Plugin URI: https://github.com/alexmoise/omx-graphics-woocommerce-customizations
  * Description: A custom plugin to add required customizations to OMX Graphics Woocommerce shop and to style the front end as required. Works based on WooCommerce Custom Fields plugin by RightPress and requires Woocommerce and Astra theme. For details/troubleshooting please contact me at <a href="https://moise.pro/contact/">https://moise.pro/contact/</a>
- * Version: 1.2.69
+ * Version: 1.2.70
  * Author: Alex Moise
  * Author URI: https://moise.pro
  * WC requires at least: 3.0.0
@@ -276,7 +276,7 @@ function moomx_save_share_cart_link() {
 	echo $save_share_cart_link_html;
 }
 // Add Save and Share cart button right after Proceed to Checkout button in Cart
-add_action( 'woocommerce_proceed_to_checkout', 'moomx_save_cart_button', 100);
+// add_action( 'woocommerce_proceed_to_checkout', 'moomx_save_cart_button', 100);
 function moomx_save_cart_button() {
 	echo '<a href="/cart/#email-cart" class="save_share_cart-button button alt wc-forward">Save & Share Cart</a>';
 }
